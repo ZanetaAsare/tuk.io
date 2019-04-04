7c0697488955010266f61f3e7172679f0b27d42a
Bruno! <bruno@localhost.localdomain>
2019-04-04 22:47:37 +0000

    fetching data

--- www/js/init.js
+++ www/js/init.js
@@ -1,3 +1,32 @@
-document.addEventListener('deviceready', () => {
-    console.log("yaaay");
-}, false);
+const config = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+
+document.addEventListener(
+  "deviceready",
+  () => {
+    handleRegister();
+  },
+  false
+);
+
+const handleRegister = () => {
+  $("#bt_register").click(() => {
+    const name = $("#name").val();
+    const email = $("#email-reg").val();
+    const role = $("#role option:selected").text();
+    const year_group = $("#year-group").val();
+    const password = $("#password").val();
+
+    console.log(name);
+    console.log(email);
+    console.log(role);
+    console.log(year_group);
+    console.log(password);
+
+    $("#reg_form").trigger("reset");
+  });
+};
